2f3499a7be0826acb4562295eb3b5224a911c8e0
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-27 21:18:04 +0530

    added basic logic

--- app.mjs
+++ app.mjs
@@ -7,3 +7,34 @@ const prompt = promptSync({sigint: true});
 const inputNum = prompt("Enter the number: ");
 
 console.log(`You have entered ${inputNum}`);
+
+// console.log(`The binary equivlent of ${inputNum} is: ${convertToBinary(inputNum)}`);
+
+convertToBinary( inputNum );
+
+function convertToBinary ( decimalNum ) {
+
+    const remainderArray = [];
+
+    let quotient = decimalNum;
+
+    let binaryNum = 0;
+
+    while ( quotient !== 0 ) {
+
+        remainderArray.push(decimalNum % 2);
+        
+        quotient = decimalNum / 2 ;
+
+    };
+
+    while ( remainderArray.length !== 0 ) {
+
+        binaryNum = binaryNum * 10 + remainderArray.pop();
+    };
+
+    // return binaryNum;
+
+    console.log(binaryNum);
+
+};
