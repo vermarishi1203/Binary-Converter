061d72c01631517d3f22f681431f358e01a4e7fc
vermarishi1203 <vermarishi1203@gmail.com>
2024-07-28 20:22:14 +0530

    code fixed

--- app.mjs
+++ app.mjs
@@ -8,33 +8,25 @@ const inputNum = prompt("Enter the number: ");
 
 console.log(`You have entered ${inputNum}`);
 
-// console.log(`The binary equivlent of ${inputNum} is: ${convertToBinary(inputNum)}`);
-
-convertToBinary( inputNum );
+console.log(`The binary equivlent of ${inputNum} is: ${convertToBinary(inputNum)}`);
 
 function convertToBinary ( decimalNum ) {
 
     const remainderArray = [];
 
-    let quotient = decimalNum;
-
     let binaryNum = 0;
 
-    while ( quotient !== 0 ) {
+    while ( decimalNum !== 0 ) {
 
-        remainderArray.push(decimalNum % 2);
-        
-        quotient = decimalNum / 2 ;
+        remainderArray.push(parseInt(decimalNum % 2));
+
+        decimalNum = parseInt(decimalNum / 2);
 
     };
 
-    while ( remainderArray.length !== 0 ) {
+    while ( remainderArray.length !== 0 ) binaryNum = binaryNum * 10 + remainderArray.pop();
 
-        binaryNum = binaryNum * 10 + remainderArray.pop();
-    };
-
-    // return binaryNum;
-
-    console.log(binaryNum);
+    return binaryNum;
 
 };
+
